8a7bd5053b370c476d0fcfe5e8d16dd093866c22
niels <i.am@nilq.dk>
2018-06-19 13:56:16 +0200

    [compiler] Local declaration

--- src/snek/interpreter/compiler.rs
+++ src/snek/interpreter/compiler.rs
@@ -1,4 +1,7 @@
 use super::*;
+use super::error::Response::Wrong;
+use super::source::Source;
+
 
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -16,15 +19,55 @@ pub struct Compiler<'c> {
   code:   Vec<Instruction>,
   consts: Vec<Value>,
   vm:     &'c mut VirtualMachine,
+
+  source: &'c Source,
 }
 
 impl<'c> Compiler<'c> {
-  pub fn new(vm: &'c mut VirtualMachine) -> Self {
+  pub fn new(vm: &'c mut VirtualMachine, source: &'c Source) -> Self {
     Compiler {
       locals: HashMap::new(),
       code:   Vec::new(),
       consts: Vec::new(),
       vm,
+
+      source,
+    }
+  }
+
+
+
+  fn declare_local(&mut self, name: &str) -> Result<u32, ()> {
+    use std::collections::hash_map::Entry;
+
+    let index = self.locals.len();
+
+    if index > self.locals.len() {
+      Err(
+        response!(
+          Wrong(format!("local overflow at `{}`", name)),
+          self.source.file
+        )
+      )
+    } else {
+      let index = index as u32;
+
+      let entry = self.locals.entry(name.to_string());
+
+      match entry {
+        Entry::Occupied(_) => Err(
+          response!(
+            Wrong(format!("redeclared local `{}`", name)),
+            self.source.file
+          )
+        ),
+
+        Entry::Vacant(vacant) => {
+          vacant.insert(index);
+
+          Ok(index)
+        }
+      }
     }
   }
 }
--- src/snek/interpreter/mod.rs
+++ src/snek/interpreter/mod.rs
@@ -2,6 +2,8 @@ pub mod value;
 pub mod vm;
 pub mod compiler;
 
+use super::*;
+
 pub use self::value::*;
 pub use self::vm::*;
 pub use self::compiler::*;
